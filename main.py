62c9572083dea38fde2dde49febcef0d37d26539
ilyasmirnov48 <ilyasmirnov48@mail.ru>
2026-01-19 21:24:14 +0300

    main commit

---
+++ main.py
@@ -0,0 +1,41 @@
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher
+from config.config import Config, load_config
+from handlers.other import other_router
+from handlers.user import user_router
+from middlewares.inner import (
+    FirstInnerMiddleware,
+    SecondInnerMiddleware,
+    ThirdInnerMiddlesare
+)
+from middlewares.outer import (
+    FirstOuterMiddleware,
+    SecondOuterMiddleware,
+    ThirdOuterMiddlesare
+)
+
+logger = logging.getLogger(__name__)
+
+
+async def main() -> None:
+
+    config: Config = load_config()
+
+    logging.basicConfig(
+        level =logging.getLevelName(level=config.log.level),
+        format=config.log.format,
+    )
+
+    bot = Bot(token=config.bot.token)
+    dp = Dispatcher()
+
+    dp.include_router(user_router)
+    dp.include_router(other_router)
+
+
+    await dp.start_polling(bot)
+
+
+asyncio.run(main())
